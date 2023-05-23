b0df38d939634da15aabbd13ec72c9d865995b7f
WORK <r200256@gmail.com>
2023-05-23 14:06:44 +0600

    Test Git

--- microblog/config.py
+++ microblog/config.py
@@ -8,3 +8,5 @@ class Config(object):
         'DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+# Test git
