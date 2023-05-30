6ba708dc43c2f1270d4a64ae61ad63c70f4ca65d
WORK <r200256@gmail.com>
2023-05-30 16:42:53 +0600

    Part_8

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
