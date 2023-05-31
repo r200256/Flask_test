2f300b179fa143f22452125edf951aa3b88be2d4
WORK <r200256@gmail.com>
2023-05-31 17:14:02 +0600

    Part_9

--- microblog/config.py
+++ microblog/config.py
@@ -7,9 +7,10 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('smtp.spaceweb.ru')    
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS= os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('555')
-    MAIL_PASSWORD = os.environ.get('16112017NoteBook')
-    admins = ['555@aromika.info']
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
+    POSTS_PER_PAGE = 25
